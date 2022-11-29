f66fd12619f5ef17d0dba9d3b074be838b4eddaf
SayHelloToKam <sayhello@kamilwatson.com>
2022-11-29 14:03:20 -0500

    added date and time functionality

--- script.js
+++ script.js
@@ -23,12 +23,19 @@ function setTime() {
     const time = new Date()
     const month = time.getMonth()
     const day = time.getDay()
+    const date = time.getDate()
     const hours = time.getHours()
-    const hoursForClock = hours % 12
+    const hoursForClock = hours >= 13 ? hours % 12 : hours;
     const minutes = time.getMinutes()
     const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate()`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class='circle'>${date}</span>`
 }
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
@@ -37,3 +44,5 @@ const scale = (num, in_min, in_max, out_min, out_max) => {
 }
 
 setTime()
+
+setInterval(setTime, 1000)
